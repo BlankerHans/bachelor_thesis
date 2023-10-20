8b0ce15468086fd440baff64001fe99734d1d3aa
Henning <90455840+BlankerHans@users.noreply.github.com>
2023-10-20 14:48:15 +0200

    Add files via upload

--- regressions_volatility_code.R
+++ regressions_volatility_code.R
@@ -11,22 +11,101 @@ data = read.csv2("regression_data_set.csv", header=TRUE, sep=",")
 #install.packages("tseries")
 #install.packages("lmtest")
 #install.packages("modelsummary")
+#install.packages("dplyr")
+#install.packages("tidyverse")
+library(tidyverse)
 library(modelsummary)
 library(tseries)
 library(lmtest)
-
+#install.packages("car")
+#install.packages("tseries")
+library(car)
+library(tseries)
+library(zoo)
+library(dplyr)
+#install.packages("dynamac")
+library(dynamac)
+#install.packages("urca")
+library(urca)
+#install.packages("TSstudio")
+library(TSstudio)
 
 # regression --------------------------------------------------------------
 
+data$date = as.Date(data$date)
+data$vola = as.numeric(data$vola)
+data$open = as.numeric(data$open)
+data$close = as.numeric(data$close)
+data$delta_price = as.numeric(data$delta_price)
 View(data)
-data = na.omit(data)
+data = data[-(1:29), ]
+rownames(data) = NULL
 View(data)
 
-# df <– as.data.frame(diff(as.matrix(data), lag = 1))
-# colnames(df) <– c(“LRM”, “LRY”, “IBO”, “IDE”)
+start = min(data$date)
+end = max(data$date)
+
+
+# filling in NA where no data entry
+# you can see in row 80 for example the is no entry for the twtr variable
+data[data == 0] = NA
+data[data == ""] = NA
+
+# interpolation for NA values
+data$btc <- na.approx(data$btc, rule=2)
+data$twtr <- na.approx(data$twtr, rule=2)
+
+View(data)
+
+# checking for dates being correct and complete
+
+full_date_range <- seq(from = min(data$date), to = max(data$date), by = "day")
+
+missing_dates <- setdiff(full_date_range, data$date)
+
+num_missing_dates <- length(missing_dates)
+
+cat("Anzahl der fehlenden Daten: ", num_missing_dates, "\n")
+cat("Fehlende Daten:\n")
+print(missing_dates)
+
+
+# ADL-model has following assumptions:
+# 1.
+# 2.
+
+# time sereis
+delta_price.ts = ts(data$delta_price, start=c(2014, 10), end=(2023, 03))
+
 
 # checking for stationarity
 
-adf.test(data$Vola)
-adf.test(data$Supply)
-adf.test(data$Tweets)
+adf.test(data$vola)
+adf.test(data$btc)
+adf.test(data$twtr)
+
+# ADL-model regression
+
+data$vola_lag1 = lag(data$vola, 1)
+data$twtr_lag1 = lag(data$twtr, 1)
+data$btc_lag1 = lag(data$btc, 1)
+data$btc_percent_change = ((data$btc - data$btc_lag1) / data$btc_lag1)*100
+data$btc_percent_change_lag1 = lag(data$btc_percent_change, 1)
+
+
+print(sum(is.na(data)))
+# NAs because of lag variables:
+data = na.omit(data)
+rownames(data) = NULL
+
+model1 = lm(vola ~ vola_lag1 + twtr + twtr_lag1 + btc + btc_lag1, data = data)
+model2 = lm(vola ~ vola_lag1 + twtr + twtr_lag1 + btc_percent_change + btc_percent_change_lag1, data = data)
+model3 = lm(vola ~ vola_lag1 + lag(vola, 2) + twtr + twtr_lag1 + lag(twtr, 2) + btc_percent_change + btc_percent_change_lag1, data = data)
+
+models = list("Standard (t-1 Lag)" = model1, "ΔBTC/BTC" = model2, "t-2 Lag" = model3)
+
+modelsummary(models, stars=c('***' = 0.01, '**' = 0.05, '*' = 0.1), gof_omit = "^(?!R2|Num)")
+#modelsummary(model1, stars=c('***' = 0.01, '**' = 0.05, '*' = 0.1), gof_omit = "^(?!R2|Num)")
+
+plot(data$vola, data$, main="Plot", xlab="Nachfrage", ylab="Vola")
+
